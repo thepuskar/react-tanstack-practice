dcc1dc8181ba721f2f5c89370ee2a2745d1f019e
Puskar Adhikari <puskar.ad@gmail.com>
2023-12-26 16:54:22 +0545

    feat: service to get product by id

--- src/service/product-service.ts
+++ src/service/product-service.ts
@@ -6,3 +6,8 @@ export async function fetchAllProducts() {
   const res = await axios.get<Product[]>(API_URL + "/products");
   return res.data;
 }
+
+export async function getProductById(id: string): Promise<Product> {
+  const response = await axios.get<Product>(API_URL + "/products/" + id);
+  return response.data;
+}
