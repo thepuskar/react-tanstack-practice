2ddf31e71cb696cff758fc5cd480b3cf122e9bfb
Puskar Adhikari <puskar.ad@gmail.com>
2023-12-26 16:54:05 +0545

    ci: types updated

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,16 +1,17 @@
 export interface Category {
-  id: number;
+  id: string;
   name: string;
   image: string;
   creationAt: string | Date;
   updatedAt: string | Date;
 }
-export interface Product extends Category {
-  id: number;
+export interface Product {
+  id: string;
   title: string;
   price: number;
   description: string;
   images: string[];
   creationAt: string | Date;
   updatedAt: string | Date;
+  category: Category;
 }
