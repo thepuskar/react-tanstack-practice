3c9ffe3bed05ae5e6ea1a04fd25a776cc57be689
Puskar Adhikari <puskar.ad@gmail.com>
2023-12-27 21:46:47 +0545

    fix: category loading skeleton

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -2,9 +2,20 @@ import { useQuery } from "@tanstack/react-query";
 import { Link } from "@tanstack/react-router";
 import { Skeleton } from "components/ui";
 import { getAllCategories } from "service";
+import { cn } from "utils";
 
-const ListSkeleton = () => {
-  return <Skeleton className="h-7 w-full bg-gray-300 rounded" />;
+const ListSkeleton = ({ className }: { className?: string }) => {
+  return (
+    <div>
+      <Skeleton className={cn("h-7 w-full bg-gray-200 rounded", className)} />
+      <Skeleton className={cn("h-7 w-full bg-gray-200 rounded", className)} />
+      <Skeleton className={cn("h-7 w-full bg-gray-200 rounded", className)} />
+      <Skeleton className={cn("h-7 w-full bg-gray-200 rounded", className)} />
+      <Skeleton className={cn("h-7 w-full bg-gray-200 rounded", className)} />
+      <Skeleton className={cn("h-7 w-full bg-gray-200 rounded", className)} />
+      <Skeleton className={cn("h-7 w-full bg-gray-200 rounded", className)} />
+    </div>
+  );
 };
 
 export const Sidebar = () => {
@@ -16,13 +27,15 @@ export const Sidebar = () => {
   return (
     <div className="relative">
       <div className="flex fixed  w-[290px] h-screen flex-col justify-between border-e ">
-        <ul className="space-y-1 pr-1">
-          {isLoading ? (
-            <ListSkeleton />
-          ) : isError ? (
-            <div>Error...</div>
-          ) : (
-            data?.map((category) => (
+        {isLoading ? (
+          <>
+            <ListSkeleton className="mb-2" />
+          </>
+        ) : isError ? (
+          <div>Error...</div>
+        ) : (
+          <ul className="space-y-1 pr-1">
+            {data?.map((category) => (
               <li key={category.id}>
                 <Link
                   to="/"
@@ -31,9 +44,9 @@ export const Sidebar = () => {
                   {category.name}
                 </Link>
               </li>
-            ))
-          )}
-        </ul>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
